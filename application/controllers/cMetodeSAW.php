d97a0b9864c78856841384a3fefbf2d3c8468677
upimariani <upimariani.uniku17@gmail.com>
2022-05-17 18:44:35 +0700

    metode saw

--- application/controllers/cMetodeSAW.php
+++ application/controllers/cMetodeSAW.php
@@ -41,37 +41,39 @@ class cMetodeSAW extends CI_Controller
             $cek = $this->mMetodeSAW->select();
             foreach ($cek as $key => $value) {
                 $id_penduduk = $value->id_penduduk;
+                var_dump($id_penduduk);
             }
             $id = $this->input->post('no_kk');
-            if ($id_penduduk == $id) {
-                $this->session->set_flashdata('error', 'Perhitungan Sudah Dilakukan Silahkan Cek Di Informasi Hasil Perhitungan!');
-                redirect('cMetodeSAW');
-            } else {
-                //perhitungan dengan menggunakan metode SAW
-                $k1 = $this->input->post('k1');
-                $k2 = $this->input->post('k2');
-                $k3 = $this->input->post('k3');
 
-                //melakukan proses normalisasi atau penyederhanaan
-                $kn1 = $k1 / 10;
-                $kn2 = $k2 / 10;
-                $kn3 = $k3 / 10;
+            // if ($id_penduduk == $id) {
+            //     $this->session->set_flashdata('error', 'Perhitungan Sudah Dilakukan Silahkan Cek Di Informasi Hasil Perhitungan!');
+            //     redirect('cMetodeSAW');
+            // } else {
+            //     //perhitungan dengan menggunakan metode SAW
+            //     $k1 = $this->input->post('k1');
+            //     $k2 = $this->input->post('k2');
+            //     $k3 = $this->input->post('k3');
 
-                //perhitungan
-                $hasil = 0.0;
-                $hasil = (10 * $kn1) + (5 * $kn2) + (7.5 * $kn3);
+            //     //melakukan proses normalisasi atau penyederhanaan
+            //     $kn1 = $k1 / 10;
+            //     $kn2 = $k2 / 10;
+            //     $kn3 = $k3 / 10;
 
-                $data = array(
-                    'id_admin' => $this->session->userdata('id'),
-                    'id_penduduk' => $this->input->post('no_kk'),
-                    'penghasilan' => $this->input->post('penghasilan'),
-                    'kondisi_rumah' => $this->input->post('rumah'),
-                    'hasil' => $hasil
-                );
-                $this->mMetodeSAW->insert($data);
-                $this->session->set_flashdata('success', 'Data Perhitungan Status Ekonomi Berhasil Ditambahkan!');
-                redirect('cMetodeSAW');
-            }
+            //     //perhitungan
+            //     $hasil = 0.0;
+            //     $hasil = (10 * $kn1) + (5 * $kn2) + (7.5 * $kn3);
+
+            //     $data = array(
+            //         'id_admin' => $this->session->userdata('id'),
+            //         'id_penduduk' => $this->input->post('no_kk'),
+            //         'penghasilan' => $this->input->post('penghasilan'),
+            //         'kondisi_rumah' => $this->input->post('rumah'),
+            //         'hasil' => $hasil
+            //     );
+            //     $this->mMetodeSAW->insert($data);
+            //     $this->session->set_flashdata('success', 'Data Perhitungan Status Ekonomi Berhasil Ditambahkan!');
+            //     redirect('cMetodeSAW');
+
         }
     }
 }
